042167f297bfcf817af0b560a69f07c16b82b411
aaraoeller <89228715+aaraoeller@users.noreply.github.com>
2023-08-30 15:35:01 -0300

    Update jogo_Adivinhacao.py

--- jogo_Adivinhacao.py
+++ jogo_Adivinhacao.py
@@ -1,17 +1,5 @@
 import random
 
-def guess(x):
-    random_number = random.randint(1,x)
-    chute = 0
-    while chute != random_number:
-        chute = int(input(f'chute um número entre 1 e {x}: '))
-        if chute < random_number:
-            print('Desculpe, chute novamente. Muito baixo.')
-        elif chute > random_number:
-            print('Desculpe, chute novamente. Muito alto.')
-    
-    print(f'Eu acertei!')
-
 def chute_computador(x):
     baixo = 1
     alto = x
@@ -21,12 +9,12 @@ def chute_computador(x):
             chute = random.randint(baixo, alto)
         else:
             chute = baixo 
-            dica = input(f'{chute} é Muito alto (H), muito baixo (L), ou correto (C)? ')
+        dica = input(f' O seu número é {chute} ? Muito alto (H), muito baixo (L), ou correto (C)? ').lower()
         if dica == 'h':
-            alto == chute - 1
+            alto = chute - 1
         elif dica == 'l':
             baixo = chute + 1
     
     print(f'Eu acertei!')
 
-chute_computador(100)
+chute_computador(1000)
