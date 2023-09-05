e39b08a3765c5d755ddaa844d88df694c0df1d99
aarao <89228715+aaraoeller@users.noreply.github.com>
2023-09-05 12:19:50 -0400

    Update jogo_Adivinhacao.py

--- jogo_Adivinhacao.py
+++ jogo_Adivinhacao.py
@@ -4,22 +4,28 @@ def chute_computador(x):
     baixo = 1
     alto = x
     dica = ''
-    chances = 10;
-    while dica != 'c':
+    chances = 10
+    tentativas = 0
+    while dica != 'c' and chances > 0:
         if baixo <= alto:
             chute = random.randint(baixo, alto)
-            chances = chances - 1
         else:
-            chute = baixo
+            print(f'Intervalo inválido: baixo ({baixo}) é maior que alto ({alto}).')
             break
-        dica = input(f' O seu número é {chute} ? Muito alto (H), muito baixo (L), ou correto (C)? ').lower()
+
+        print(f'Computador: Eu tenho {chances} chances.')
+        dica = input(f'O seu número é {chute} ? Muito alto (H), muito baixo (L), ou correto (C)? ').lower()
         if dica == 'h':
-            if chute < alto:
-                alto = chute - 1
-            else:
-                baixo = chute + 1
+            alto = chute - 1
         elif dica == 'l':
             baixo = chute + 1
-    print(f'Eu já sabia, só estava te testando!')
+        chances -= 1
+        tentativas += 1
+
+    if dica == 'c':
+        print(f'Computador: Eu adivinhei, o seu número é ({chute})')
+        print(f'Computador: Foram necessárias {tentativas} tentativas para acertar.')
+    else:
+        print(f'Computador: As minhas chances se esgotaram. Mas eu acho que o número era {x}.')
 
 chute_computador(100)
