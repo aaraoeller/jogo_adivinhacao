b854d99e4ddf52cdc2b94908f25a5ce611efa4f5
aaraoeller <89228715+aaraoeller@users.noreply.github.com>
2023-10-08 18:42:44 -0400

    testeando

---
+++ Teste.py
@@ -0,0 +1,26 @@
+import random
+
+def chtue_computador():
+    baixo = 0
+    alto = 100
+    dica = ''
+
+    chute = random.randint(baixo, alto)
+
+    while dica != 'c':
+
+        dica = input(f'O seu número é {chute}? Muito alto (H), Muito baixo (L), ou correto (C)?').lower()
+
+        if dica == 'h':
+            alto = chute
+        elif dica == 'l':
+            baixo = chute
+
+        chute = random.randint(baixo, alto)
+
+    if dica == 'c':
+        print(f'Eu adivinhei, o seu número é {chute}')
+    else:
+        print('Minhas chances se esgotaram')
+
+chtue_computador()
